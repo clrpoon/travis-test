002f47d4cc8d9248f09f4b69856ed1260b5288f5
Casey Poon <caseypoon2020@u.northwestern.edu>
2020-03-18 15:03:31 -0500

    travis test

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import SplashPage from './Pages/SplashPage';
+import WorkoutsPage from './Pages/WorkoutsPage';
+import TopMenu from './Components/TopMenu';
+import ProfilePage from './Pages/ProfilePage';
+import { firebase }from './firebaseDb';
+import 'firebase/database';
+import "firebase/auth";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
+  
+
+
+
+  const withMenu = (page) => {
+    return (
+      <Container>
+        <TopMenu user={user} />
+        {page}
+      </Container>
+    )
+  };
+
+  return (
+    <BrowserRouter>
+      <Route 
+        exact 
+        path='/' 
+        render={() => withMenu(<SplashPage />)}
+      />
+      <Route
+        path='/workouts'
+        render={() => withMenu(<WorkoutsPage user={user} />)}
+      />
+      <Route
+        path='/profile'
+        render={() => withMenu(<ProfilePage user={user} />)}
+      />
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/Components/EquipWindow.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Header, Image, Modal } from "semantic-ui-react";
+
+const EquipWindow = ({ name, description, url }) => (
+  <Modal
+    size="tiny"
+    closeIcon
+    trigger={
+      <Button
+        style={{ boxShadow: "none" }}
+        icon="question circle"
+        size="tiny"
+        circular
+        basic
+        data-testid="newItemField"
+      />
+    }
+  >
+    <Modal.Header data-testid="newName">{name}</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size="medium" src={url} />
+      <Modal.Description>
+        <Header>About</Header>
+        <p data-testid="newDesc">{description}</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
+);
+
+export default EquipWindow;
